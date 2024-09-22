57cb0a63226a4259447acd7dfa3042c52a87c861
arielweb60897 <ariel60897@gmail.com>
2024-09-22 09:59:27 +0800

    add

--- JavaScript/case study/表單驗證/new.js
+++ JavaScript/case study/表單驗證/new.js
@@ -1,17 +1,32 @@
-function doFirst(){
-    // 跟 HTML 畫面產生關連，再建事件聆聽功能
-    document.getElementById('theForm').onsubmit = validate
+function doFirst() {
+  // 跟 HTML 畫面產生關連，再建事件聆聽功能
+  document.getElementById("theForm").onsubmit = validate;
 }
-function validate(e){
-    e.preventDefault()
-    let firstName = document.getElementById('firstName')
-    let lastName = document.getElementById('lastName')
-    let username = document.getElementById('username')
-    let password = document.getElementById('password')
-    let email = document.getElementById('email')
-    let phone = document.getElementById('phone')
-    let gender = document.getElementById('gender')
+function validate(e) {
+  e.preventDefault();
+  let firstName = document.getElementById("firstName");
+  let lastName = document.getElementById("lastName");
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  let email = document.getElementById("email");
+  let phone = document.getElementById("phone");
+  let gender = document.getElementById("gender");
+  //參考【正規表示法表】
+  if (/^[A-Za-z]{1,15}$/.test(firstName.value)) {
+    alert = "first name pass";
+  } else {
+    alert = "first name xx";
+  }
+  if (/^[A-Za-z0-9]{6,12}$/.test(username.value)) {
+    alert = "username pass";
+  } else {
+    alert = "username xx";
+  }
 
-   
+  if (/^[A-Za-z0-9_.-]+@[A-Za-z0-9]+\.[A-Za-z]{2,6}$/.test(email.value)) {
+    alert(`email pass`);
+  } else {
+    alert(`email XX`);
+  }
 }
-window.addEventListener('load',doFirst)
+window.addEventListener("load", doFirst);
